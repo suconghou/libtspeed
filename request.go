07822175c1cbac793c80e5f22fb65d4e7fb57d21
suconghou <1126045770@qq.com>
2017-11-26 00:39:47 +0800

    update progress

--- request.go
+++ request.go
@@ -5,28 +5,25 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"time"
 
 	"github.com/suconghou/utilgo"
 )
 
 type writeCounter struct {
-	readed    int64
-	total     int64
-	startTime time.Time
-	origin    io.Reader
+	readed   int64
+	total    int64
+	progress func(received int64, readed int64, total int64, start int64, end int64)
+	origin   io.Reader
 }
 
 func (wc *writeCounter) Read(p []byte) (int, error) {
 	n, err := wc.origin.Read(p)
 	wc.readed += int64(n)
-	progress := utilgo.ProgressBar("", "", nil, nil)
-	progress(wc.readed, wc.readed, wc.total, time.Since(wc.startTime).Seconds(), 0, wc.total)
+	wc.progress(wc.readed, wc.readed, wc.total, 0, wc.total)
 	return n, err
 }
 
 func benchmark(url string, thunk uint, timeout uint, transport *http.Transport) error {
-	startTime := time.Now()
 	resp, err := utilgo.Dohttp(url, "GET", nil, nil, timeout, transport)
 	if err != nil {
 		return err
@@ -41,7 +38,7 @@ func benchmark(url string, thunk uint, timeout uint, transport *http.Transport) 
 		} else {
 			r = io.LimitReader(resp.Body, total)
 		}
-		_, err = io.Copy(ioutil.Discard, &writeCounter{origin: r, total: total, startTime: startTime})
+		_, err = io.Copy(ioutil.Discard, &writeCounter{origin: r, total: total, progress: utilgo.ProgressBar("", "", nil, nil)})
 		return err
 	}
 	return fmt.Errorf("%d:%s Length %d", resp.StatusCode, resp.Status, resp.ContentLength)
